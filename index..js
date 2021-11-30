19105c0716d61acc2334ae83f01ffa5c7a625b99
vbachdoan <75577240+vbachdoan@users.noreply.github.com>
2021-11-30 23:19:25 +0700

    fix word

--- index..js
+++ index..js
@@ -9,9 +9,9 @@ document.getElementById("btn").addEventListener("click",function(){
     document.getElementById("text").value=""
     select = Math.floor(Math.random() * 3 )
     switch(select){
-        case 0: document.getElementById("output-text").innerText='Gọi '+ name +' là cây xăng vì lúc nào nhìn thấy em anh cũng muốn đổ.'
+        case 0: document.getElementById("output-text").innerText='Gọi '+ name +' là cây xăng vì lúc nào nhìn thấy '+ name +' anh cũng muốn đổ.'
             break;
-        case 1: document.getElementById("output-text").innerText="Hey "+ name +'. Nếu chúng ta cùng đóng Táo Quân, thì anh sẽ là Nam Tào còn em sẽ là Co Dau nhé.'
+        case 1: document.getElementById("output-text").innerText="Hey "+ name +'. Nếu chúng ta cùng đóng Táo Quân, thì anh sẽ là Nam Tào còn '+ name +' sẽ là Co Dau nhé.'
             break;
         case 2: document.getElementById("output-text").innerText="Hey "+ name +'. Nếu có 2 con cua, 1 to và 1 nhỏ. Tớ là con cua nhỏ còn cau la cua to.'
             break;
